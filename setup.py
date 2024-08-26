75e9a6d4cd42ff98f332cd680b691080173657cd
Juan Sebastian <juansepc@hotmail.com>
2024-08-26 17:12:42 -0500

    commit tecero

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_package_2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jagger',
+    maintainer_email='juansepc@hotmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node_2 = my_package_2.my_node_2:main'
+        ],
+    },
+)
