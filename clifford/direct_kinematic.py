9a393fd0f25c9077ad3b2a1797eecab562461f70
Juan Sebastian <juansepc@hotmail.com>
2024-10-13 18:44:05 -0500

    Cambio de la libreria simpy a numpy para el dh

--- clifford/direct_kinematic.py
+++ clifford/direct_kinematic.py
@@ -1,53 +1,80 @@
 import rclpy
 
 from rclpy.node import Node
-from std_msgs.msg import String
 from sensor_msgs.msg import JointState
 
 import math
 import numpy as np
-import sympy as sp
 
 
 class directKinematic(Node):
     def __init__(self):
         super().__init__('cinematica_directa')
 
-        self.theta1 = sp.symbols('q1') 
-        self.theta2 = sp.symbols('q2') 
-        self.theta3 = sp.symbols('q3') 
+        self.theta_mundo = np.radians(0)
+        self.theta1 = np.radians(0)  
+        self.theta2 = np.radians(0)
+        self.theta3 = np.radians(0)
 
-        #self.d1 = sp.symbols('d1')
-        #self.d2 = sp.symbols('d2')
-        #self.d3 = sp.symbols('d3') 
+        self.d_mundo = 0
+        self.d1 = 0
+        self.d2 = 0
+        self.d3 = 0
 
-        self.a1 = sp.symbols('l1') 
-        self.a2 = sp.symbols('l2') 
-        self.a3 = sp.symbols('l3')
+        self.a_mundo = 0
+        self.a1 = 1 
+        self.a2 = 1
+        self.a3 = 1
 
-        #self.alpha1 = sp.symbols('a1')
-        #self.alpha2 = sp.symbols('a2')
-        #self.alpha3 = sp.symbols('a3')
+        self.alpha1 = np.radians(-90)
+        self.alpha2 = np.radians(0)
+        self.alpha3 = np.radians(0)
 
         self.publisher_= self.create_publisher(JointState, '/joint_states', 10)
+
+        self.T_new_y = np.array([[np.cos(np.radians(90)), 0, np.sin(np.radians(90)), 0],
+                                 [0, 1, 0, 0],
+                                 [-np.sin(np.radians(90)), 0, np.cos(np.radians(90)), 0],
+                                 [0, 0, 0, 1]])
         
-        self.T1 = self.matriz_dh(self.theta1, self.a1)
-        self.T2 = self.matriz_dh(self.theta2, self.a2)
-        self.T3 = self.matriz_dh(self.theta3, self.a3)
+        self.T1 = self.denavit(self.theta1, self.d1, self.a1, self.alpha1)
+        self.T2 = self.denavit(self.theta2, self.d2, self.a2, self.alpha2)
+        self.T3 = self.denavit(self.theta3, self.d3, self.a3, self.alpha3)
 
-        self.T_total = self.T1 * self.T2 * self.T3
-        self.T_total_simp = sp.simplify(self.T_total)
+        self.T_total = np.dot(np.dot(self.T1, self.T2), self.T3)
+        self.T_total = np.dot(self.T_new_y, self.T_total)
+
+        self.PX = self.T_total[0,3] 
+        self.PY = self.T_total[1,3]
+        self.PZ = self.T_total[2,3]
+
+        self.get_logger().info(f"{self.T_total}")
+        self.get_logger().info("PARA LA ULTIMA COLUMNA FILA 1, 2, 4")
+        self.get_logger().info(f"{self.PX, self.PY, self.PZ}")
+
+        print("\n")
+        print("\n")
+        print(self.PX)
+        print(self.PY)
+        print(self.PZ)
+
+
+    def denavit(self, teta, d, a, alfa):
+        dh = np.array([[np.cos(teta), -np.cos(alfa)*np.sin(teta), np.sin(alfa)*np.sin(teta), a*np.cos(teta)],
+                      [np.sin(teta), np.cos(alfa)*np.cos(teta), -np.sin(alfa)*np.cos(teta), a*np.sin(teta)],
+                      [0, np.sin(alfa), np.cos(alfa), d],
+                      [0, 0, 0, 1]])
         
-        self.get_logger().info(f"{self.T_total_simp}")
+        return dh
 
+    def send_attributes(self):
 
-    def matriz_dh(self, theta, a ):
-        return sp.Matrix([
-        [sp.cos(theta), -sp.sin(theta), 0, a*sp.cos(theta)],
-        [sp.sin(theta), sp.cos(theta),  0, a*sp.sin(theta)],
-        [0,             0,              1, 0],
-        [0,             0,              0, 1]
-    ])
+        PX = self.PX
+        PY = self.PY
+        PZ = self.PZ
+
+        return PX, PY, PZ
+
 
 
 def main(args=None):
